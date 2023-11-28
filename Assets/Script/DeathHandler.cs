0efca05e24042572baf1a3b5c973fc7820c4f5d1
Chaemin-Lim <antraxmin@naver.com>
2023-11-29 07:28:10 +0900

    feat: 플레이어가 죽었을때 종료 UI 활성화

--- Assets/Script/DeathHandler.cs
+++ Assets/Script/DeathHandler.cs
@@ -11,4 +11,17 @@ public class DeathHandler : MonoBehaviour
         // 게임 시작할 때 종료 UI가 나타나지 않도록
         gameOverCanvas.enabled = false;
     }
+
+    // 플레이어가 죽었을때 종료 UI 활성화
+    public void HandleDeath()
+    {
+        gameOverCanvas.enabled = true;
+        Time.timeScale = 0;
+
+        // 마우스 클릭 활성화
+        Cursor.lockState = CursorLockMode.None;
+
+        // 사용자가 마우스 확인 가능하도록
+        Cursor.visible = true;
+    }
 }
