1aa90decd025124651b831b773e783dc9a160145
Chaemin-Lim <antraxmin@naver.com>
2023-12-04 15:57:58 +0900

    feat: 싱글플레이 scene 전환 스크립트 추가

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public void SinglePlayStart()
+    {
+        // SinglePlayScene으로 전환
+        SceneManager.LoadScene("SinglePlayScene");
+    }
     public void MultiPlayStart()
     {
         // LobbyScene으로 전환
